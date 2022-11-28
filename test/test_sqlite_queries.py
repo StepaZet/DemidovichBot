3be83f361a6e66891859f6f73a91a52cda9ca4a4
Ilya Kovalchuk <sckremyulock@gmail.com>
2022-11-29 00:49:02 +0500

    add sqlite wrapper

---
+++ test/test_sqlite_queries.py
@@ -0,0 +1,34 @@
+import unittest
+import sqlite3
+
+
+class TestSQLite(unittest.TestCase):
+    def setUp(self) -> None:
+        sqlite_connection = sqlite3.connect('sqlite_test.db')
+        self.__cursor = sqlite_connection.cursor()
+
+    def test_get_non_existent_table(self):
+        with self.assertRaises(sqlite3.OperationalError):
+            self.__cursor.execute('SELECT * FROM non_existent_table')
+
+    def test_create_table(self):
+        table_name = 'test_table'
+        self.__cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
+        self.__cursor.execute(f'CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, value TEXT)')
+        self.__cursor.execute(f'SELECT * FROM {table_name}')
+        self.assertEqual(self.__cursor.fetchall(), [])
+
+    def test_add_value(self):
+        table_name = 'test_table'
+        self.__cursor.execute(f'INSERT INTO {table_name} VALUES (3, "test3")')
+        self.__cursor.execute(f'SELECT * FROM {table_name}')
+        self.assertEqual(self.__cursor.fetchone(), (3, 'test3'))
+
+    def test_delete_value_from_table(self):
+        table_name = 'test_table'
+        self.__cursor.execute(f'INSERT INTO {table_name} VALUES (2, "test2")')
+        self.__cursor.execute(f'SELECT * FROM {table_name}')
+        self.assertIsNotNone(self.__cursor.fetchone())
+        self.__cursor.execute(f'DELETE FROM {table_name} WHERE id = 2')
+        self.__cursor.execute(f'SELECT * FROM {table_name}')
+        self.assertIsNone(self.__cursor.fetchone())
